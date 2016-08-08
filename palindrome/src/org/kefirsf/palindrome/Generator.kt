96bc0d5797a326cc626bedc7788a5ddfc4143fcc
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2016-08-08 20:42:49 +0500

    completed algorithm

--- palindrome/src/org/kefirsf/palindrome/Generator.kt
+++ palindrome/src/org/kefirsf/palindrome/Generator.kt
@@ -25,40 +25,76 @@ class Generator {
             val newGen = HashSet<Candidate>()
 
             for (c in oldGen) {
-                if (c.leftSize > c.rightSize) {
-                    for (sp in (1..(c.part.length - 1)).map { c.part.take(it) }) {
-                        val word = reversed[sp]
-                        if (word != null) {
-                            newGen.add(Candidate(c, word))
-                        }
-                    }
+                val children = generateChildren(c)
 
-                    for ((key, value) in reversed.tailMap(c.part)) {
-                        if (key.startsWith(c.part)) {
-                            newGen.add(Candidate(c, value))
-                        } else {
-                            break
-                        }
-                    }
-                } else if (c.leftSize < c.rightSize) {
-
-                }
+                palindromes.addAll(children.filter { it.palindrome }.map { it.result })
+                newGen.addAll(children.filterNot { it.palindrome })
             }
 
-            // TODO remove
-            System.out.println(newGen)
-
-            palindromes.addAll(newGen.filter { it.palindrome }.map { it.result })
-            oldGen = newGen.filterNot { it.palindrome }
-
-            if(oldGen.isEmpty()){
+            if (newGen.isEmpty()) {
                 break
             }
+
+            oldGen = newGen
         }
     }
 
-    val count:Int
-    get(){
-        return palindromes.size
+    private fun generateChildren(c: Candidate): HashSet<Candidate> {
+        if (c.leftSize > c.rightSize) {
+            return generateRightChildren(c)
+        } else if (c.leftSize < c.rightSize) {
+            return generateLeftChildren(c)
+        } else {
+            throw ImpossibleWayException();
+        }
     }
+
+    private fun generateLeftChildren(c: Candidate): HashSet<Candidate> {
+        val newCandidates = HashSet<Candidate>()
+        for (sp in (1..(c.part.length - 1)).map { c.part.takeLast(it) }) {
+            val word = sp.reversed()
+            if (direct.contains(word)) {
+                newCandidates.add(Candidate(c, word))
+            }
+        }
+
+        val rp = c.part.reversed()
+        for (word in direct.tailSet(rp)) {
+            if (word.startsWith(rp)) {
+                newCandidates.add(Candidate(c, word))
+            } else {
+                break;
+            }
+        }
+        return newCandidates
+    }
+
+    private fun generateRightChildren(c: Candidate): HashSet<Candidate> {
+        val newCandidates = HashSet<Candidate>()
+        for (sp in (1..(c.part.length - 1)).map { c.part.take(it) }) {
+            val word = reversed[sp]
+            if (word != null) {
+                newCandidates.add(Candidate(c, word))
+            }
+        }
+
+        for ((key, value) in reversed.tailMap(c.part)) {
+            if (key.startsWith(c.part)) {
+                newCandidates.add(Candidate(c, value))
+            } else {
+                break
+            }
+        }
+        return newCandidates
+    }
+
+    val count: Int
+        get() {
+            return palindromes.size
+        }
+}
+
+class ImpossibleWayException : Exception(
+        "This way is impossible. If the exception was thrown then there is a bug in logic."
+) {
 }
--- palindrome/test/org/kefirsf/palindrome/GeneratorTest.kt
+++ palindrome/test/org/kefirsf/palindrome/GeneratorTest.kt
@@ -8,11 +8,46 @@ import org.junit.Assert.*
  * @author kefir
  */
 class GeneratorTest {
-    @Test fun simple(){
+    @Test
+    fun simple(){
         val words : Collection<String> = listOf("манит", "аргентина", "негра")
         val gen = Generator(words)
         gen.run()
         assertEquals(1, gen.count)
         assertEquals(listOf("аргентина", "манит", "негра"), gen.palindromes.first())
+    }
+
+    @Test
+    fun bigLeft(){
+        val gen = Generator(listOf("21", "43", "65", "1234567"))
+        gen.run()
+        assertEquals(1, gen.count)
+        assertEquals(listOf("1234567", "65", "43", "21"), gen.palindromes.first())
+    }
+
+    @Test
+    fun bigRight(){
+        val gen = Generator(listOf("12", "34", "56", "7654321"))
+        gen.run()
+        assertEquals(1, gen.count)
+        assertEquals(listOf("12", "34", "56", "7654321"), gen.palindromes.first())
+    }
+
+    @Test
+    fun mirror(){
+        val gen = Generator(listOf("123", "321"))
+        gen.run()
+        assertEquals(2, gen.count)
+        assertTrue(gen.palindromes.contains(listOf("123", "321")))
+        assertTrue(gen.palindromes.contains(listOf("321", "123")))
+    }
+
+    @Test
+    fun chain(){
+        val gen = Generator(listOf("123", "0987", "654321", "456789", "82", "010987"))
+        gen.run()
+        assertEquals(2, gen.count)
+        assertTrue(gen.palindromes.contains(listOf("123", "456789", "0987", "654321")))
+        assertTrue(gen.palindromes.contains(listOf("123", "456789", "010987", "654321")))
     }
 }
