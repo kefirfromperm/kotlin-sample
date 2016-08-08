e932305332b0ac8773c56f7c729fe069a5362ec8
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2016-08-08 22:45:10 +0500

    main

--- palindrome/src/org/kefirsf/palindrome/Main.kt
+++ palindrome/src/org/kefirsf/palindrome/Main.kt
@@ -1,5 +1,16 @@
 package org.kefirsf.palindrome
 
+import java.io.File
+import java.nio.charset.Charset
+
 /**
  * @author kefir
  */
+fun main(args:Array<String>){
+    val gen = Generator(File("e:/tmp/words.txt").readLines(Charset.forName("cp1251")))
+    //gen.setOutput(System.out)
+    gen.run(5)
+    gen.palindromes.forEach {
+        System.out.println(it)
+    }
+}
